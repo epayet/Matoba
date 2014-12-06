2fb87ce53c21888ff67ba4f26b0727a8544adc9d
xoo <bchevriaut@gmail.com>
2014-12-06 19:56:08 +0100

    Jour + nuit avec rotation

---
+++ Assets/DayNight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNight : MonoBehaviour {
+
+	private Time time;
+	public int DayDuration = 30;
+	public Light Soleil;
+	public Light Lune;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, Time.deltaTime * 50);
+		if (transform.rotation.eulerAngles.z > 100 && transform.rotation.eulerAngles.z < 260) {
+			Soleil.enabled = true;
+			Lune.enabled = false;
+		} 
+		else {
+			Soleil.enabled = false;
+			Lune.enabled = true;
+		}
+	}
+}
