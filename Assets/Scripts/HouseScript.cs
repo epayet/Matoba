3083ec97695153cc09567b1fc536f7081ecf0170
Thomas LOUBIOU <thomas.loubiou@yahoo.com>
2014-12-07 01:13:20 +0100

    Script maison + UI OK

---
+++ Assets/Scripts/HouseScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseScript : MonoBehaviour {
+
+	public GameObject unit;
+
+	public void CreateArcher() {
+		Instantiate(unit, gameObject.transform.position, gameObject.transform.rotation);
+	}
+}
