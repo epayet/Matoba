ff9c34c0e5c042c5a85722e62ac5f060bdbf3383
xoo <bchevriaut@gmail.com>
2014-12-07 17:34:57 +0100

    UI terminée pour les skills. ajout musique

---
+++ Assets/LastSkill.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LastSkill : MonoBehaviour {
+	Text instruction;
+	public Text one;
+	public Text two;
+	public Text three;
+	
+	void Start () {
+		GetComponentInParent<Button> ().enabled = false;
+		instruction = GetComponent<Text> ();
+	}
+	
+	bool AllOk(){
+		// oups
+		return one.text == "X" && two.text == "X" && three.text == "X";
+	}
+
+	void Update(){
+		if(AllOk())
+			GetComponentInParent<Button> ().enabled = true;
+	}
+
+	public void Barre(){
+		if (AllOk()) {
+			GetComponentInParent<Button> ().enabled = true;
+			instruction.text = "X";
+			GetComponentInParent<Button> ().enabled = false;
+		}
+	}
+}
---
+++ Assets/TalentsGestion.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalentsGestion : MonoBehaviour {/*
+	int[] attack;
+	int[] defense;
+	int[] others;*/
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (false);/*
+		attack = new int[]{0,0,0};
+		defense = new int[]{0,0,0};
+		others = new int[]{0,0,0};*/
+	}
+
+	public void AddAttack(int value){
+		//attack [value] += 1;
+	}
+
+	public void Show(){
+		gameObject.SetActive (true);
+	}
+
+	public void Hide(){
+		gameObject.SetActive (false);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
