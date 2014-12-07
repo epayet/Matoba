78f949a407c0681a6776bc003a065928207dc58b
epayet <jakspinning@gmail.com>
2014-12-07 14:50:42 +0100

    Maison + Spawn

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject ArcherPrefab;
+	public bool enemy;
+
+	public void CreateArcher() {
+		CreateUnit(ArcherPrefab);
+	}
+
+	private void CreateUnit(GameObject unite)
+	{
+		GameObject archer = (GameObject)Instantiate(unite, gameObject.transform.position, gameObject.transform.rotation);
+		archer.GetComponent<UniteBehaviour>().vaADroite = !enemy;
+		archer.tag = enemy ? "unit_team_2" : "unit_team_1";
+	}
+}
